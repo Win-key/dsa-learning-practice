cbc941896cf09a871335e1daf525eacbee7e21d8
Venkatesh Rajendran <ven2701@gmail.com>
2021-05-30 14:28:45 +0530

    Arrays and String: Palindrome permutation with bit manipulation

--- src/Y2021/cci/arraysandstring/PalindromePermutation.java
+++ src/Y2021/cci/arraysandstring/PalindromePermutation.java
@@ -3,12 +3,19 @@ package Y2021.cci.arraysandstring;
 public class PalindromePermutation {
 
     public static void main(String[] args) {
-        String s = "Tact Coa";
-        boolean f = checkPalindromePermutation(s);
-        System.out.println(s+" is permutation of palindrome ? " + f);
-        s = "Tact Coat";
-        f = checkPalindromePermutation(s);
-        System.out.println(s+" is permutation of palindrome ? " + f);
+        String s1 = "Tact Coa";
+        boolean f = checkPalindromePermutation(s1);
+        System.out.println(s1+" is permutation of palindrome ? " + f);
+        String s2 = "Tact Coat";
+        f = checkPalindromePermutation(s2);
+        System.out.println(s2+" is permutation of palindrome ? " + f);
+        f = checkPalindromePermutationV2(s1);
+        System.out.println(s1+" is permutation of palindrome with bits ? " + f);
+        f = checkPalindromePermutationV2(s2);
+        System.out.println(s2+" is permutation of palindrome with bits ? " + f);
+        String s3 = "Dad";
+        f = checkPalindromePermutationV2(s3);
+        System.out.println(s3+" is permutation of palindrome with bits ? " + f);
     }
 
     public static boolean checkPalindromePermutation(String s) {
@@ -41,6 +48,55 @@ public class PalindromePermutation {
                 return false;
         }
         return true;
+    }
+
+    /**
+     * If the string contains only alphabets, we can use bit manipulation
+     * @param s
+     * @return
+     */
+    public static boolean checkPalindromePermutationV2(String s) {
+        // fixme : how to avoid toLoweCasing?
+        // s = s.toLowerCase();
+        // create ascii array
+        int checker = 0;
+
+        // scan through the array and invert the flag
+        // odd chars will be set to true
+        // ignore space
+        for (char c : s.toCharArray()) {
+            // handle upper case letters
+            if(64 < c && c < 91){
+                c = (char) (c + 32);
+            }
+            if(c != ' ') {
+                // normalize to 0
+                c = (char) (c - 'a');
+                // shift 1 for c 1 << c
+                // do xor operation
+                checker ^= 1 << c;
+            }
+        }
+        // count chars with true flag
+        // more than 1 means not a palindrome
+        // need to check checker if it has more one 1 bit?
+        // 1 - 31 any one of this place should have 1 bit
+        // Use getBit methods
+        // Iterate from 0 to 25 and find if it has more than 1 bit
+        int trueCount = 0;
+        for (int i = 0; i < 26; i++) {
+            if(hasOne(checker, i)) {
+                trueCount++;
+            }
+            if (trueCount>1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean hasOne(int num, int shifter) {
+        return (num & 1 << shifter) != 0;
     }
 
 }
