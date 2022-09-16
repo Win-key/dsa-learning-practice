da214d56c351bf47b55cca6f9d4914aa013d51fd
Venkatesh Rajendran <ven2701@gmail.com>
2022-09-17 01:04:28 +0530

    Design diagrams

--- src/designPatterns/structural/composite/MainClass.java
+++ src/designPatterns/structural/composite/MainClass.java
@@ -11,14 +11,19 @@ public class MainClass {
         box1.add(new Product("iPhone 13", 140000));
         box1.add(new Product("iPhone charger", 2000));
 
+        Utils.print("Price of box 1 = " + box1.getPrice());
+
         shoppingKart.add(box1);
         shoppingKart.add(new Product("iPhone case", 1000));
-
 
         Box box2 = new Box();
         box2.add(new Product("Apple watch", 30000));
         box2.add(new Product("Gift card", 200));
+        Utils.print("Price of box 2 = " + box2.getPrice());
+
         shoppingKart.add(box2);
+
+        shoppingKart.move();
 
         double price = shoppingKart.getPrice();
 
--- src/general/GraphStatus.java
+++ src/general/GraphStatus.java
@@ -3,6 +3,11 @@ package general;
 public class GraphStatus {
 
 
+    public static void main(String[] args) {
+
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+
 //    public String findGraphStatus(int[] ar, int percentage) {
 //
 //    }
